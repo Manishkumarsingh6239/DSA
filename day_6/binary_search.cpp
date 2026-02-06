c8f0d31accc1ecf40667d9815dceb0c8a8b6ee72
Manishkumarsingh6239 <manishkumarsingh6239@gmail.com>
2026-02-06 09:11:09 +0530

    first question done of day_6

---
+++ day_6/binary_search.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low=0, high = nums.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target) high=mid-1;
+            else low=mid+1;
+        }
+        return -1;
+    }
+};
+
+int main(){
+    vector <int> arr = {-1,0,3,6,7,10};
+    Solution sol;
+    int target = 3;
+    int index = sol.search(arr,target);
+    cout<<target<<" found at "<<index<<endl;
+}
