a5375138f16c0d40e4491ee25bb77f94fb24f666
Manishkumarsingh6239 <manishkumarsingh6239@gmail.com>
2026-02-08 20:14:51 +0530

    First question of day_7

---
+++ day_7/max_height_of_binary_tree.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==nullptr) return 0;
+        return 1+max(maxDepth(root->left),maxDepth(root->right));
+    }
+};
+
+int main(){
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->right = new TreeNode(4);
+    Solution sol;
+    cout<<"The height of the given tree is "<<sol.maxDepth(root)<<endl;
+}
