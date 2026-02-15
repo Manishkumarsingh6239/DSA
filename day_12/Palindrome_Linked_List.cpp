4f152803670a476449c8094aa0e94d01568ca757
Manishkumarsingh6239 <manishkumarsingh6239@gmail.com>
2026-02-16 00:03:43 +0530

    day_12 first question

---
+++ day_12/Palindrome_Linked_List.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int> stack;
+        ListNode* curr = head;
+        while (curr) {
+            stack.push(curr->val);
+            curr = curr->next;
+        }
+        curr = head;
+        while (curr && curr->val == stack.top()) {
+            stack.pop();
+            curr = curr->next;
+        }
+        return curr == nullptr;
+    }
+};
