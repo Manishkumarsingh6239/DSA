f8e343e35a40afe6b5ca1b63ff5eb20fe98f0ada
Manishkumarsingh6239 <manishkumarsingh6239@gmail.com>
2026-02-16 00:16:00 +0530

    .

--- day_12/Palindrome_Linked_List.cpp
+++ day_12/Palindrome_Linked_List.cpp
@@ -1,5 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
+struct ListNode{
+    int val;
+    ListNode* next;
+
+    ListNode(int x=0, ListNode *next=nullptr) : val(x), next(next) {} 
+};
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
