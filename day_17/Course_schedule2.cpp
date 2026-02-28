108a9eb488d7d27907b50a392672b07089c27757
Manishkumarsingh6239 <manishkumarsingh6239@gmail.com>
2026-02-28 23:11:17 +0530

    day 17 question 2

---
+++ day_17/Course_schedule2.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adj[numCourses];
+        for(auto it : prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+
+        vector<int> indegree(numCourses, 0);
+        for(int i=0; i<numCourses; i++){
+            for(auto it : adj[i]){
+                indegree[it]++;
+            }
+        }
+
+        queue<int> q;
+        for(int i=0; i<numCourses; i++){
+            if (indegree[i]==0) q.push(i);
+        }
+        vector<int> topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+        if(topo.size()==numCourses) return topo;
+        return {};
+    }
+
+};
+
+int main(){
+    int numCourses = 2;
+    vector<vector<int>> prerequisites = {{1,0}};
+    Solution s;
+    vector<int> ans = s.findOrder(numCourses, prerequisites);
+    for(auto it : ans) cout<<it<<" ";
+}
