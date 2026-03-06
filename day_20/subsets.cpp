615cd349b37ca671ac82165fb2b2eba1631e7a28
Manishkumarsingh6239 <manishkumarsingh6239@gmail.com>
2026-03-06 21:10:00 +0530

    day 20 second question

---
+++ day_20/subsets.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// USING RECURSION
+
+// class Solution {
+// public:
+//     vector<vector<int>> ans;
+//     void solve(int index,vector<int>& subset,vector<int> nums,int n){
+//         if(index>=n){
+//             ans.push_back(subset);
+//             return;
+//         }
+//         subset.push_back(nums[index]);
+//         solve(index+1,subset,nums,n);
+//         subset.pop_back();
+//         solve(index+1,subset,nums,n);
+//     }
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<int> subset;
+//         solve(0,subset,nums,nums.size());
+//         return ans;
+//     }
+// };
+
+
+// USING BIT MANIPULATION
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        for(int num=0;num<(pow(2,n));num++){
+            vector<int> subset;
+            for(int i=0;i<n;i++){
+                if(num&(1<<i)){
+                    subset.push_back(nums[i]);
+                }
+            }
+            ans.push_back(subset);
+        }
+        return ans;
+    }
+};
+
+int main(){
+    Solution sol;
+    vector<int> nums = {1,2,3};
+    vector<vector<int>> ans = sol.subsets(nums);
+    for(auto row:ans){
+        for(auto col:row){
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+}
